92481636935008ac1c56bb254d7570541c6790a7
kucinggendut <ferd.dwi14@gmail.com>
2016-10-20 08:30:48 +0700

    Add files via upload

---
+++ bingo.java
@@ -0,0 +1,16 @@
+public class NRP5216100050 {
+	public static void main(String[] args){
+	int count = 1;
+	do{
+		if (count%2==0){
+			System.out.println("Bingo!");
+		} 
+		else 
+		{
+			System.out.println(count);
+		}
+		count++;
+	} 	while (count <10);
+			System.out.println("Bingo!");
+	}
+}
